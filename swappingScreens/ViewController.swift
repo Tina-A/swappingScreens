d4133a83dbe18b5ac3d38a6284bec06238fd3cdf
Tina Isienyi <tinaisienyi@tinas-mbp>
2017-04-25 19:22:31 +0100

    Initial Commit

---
+++ swappingScreens/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swappingScreens
+//
+//  Created by Tina Isienyi on 25/04/2017.
+//  Copyright © 2017 Tina Isienyi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
