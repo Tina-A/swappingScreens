4fcfaecc70e8533a620212a18e0448e6b35e80c7
Tina Isienyi <tinaisienyi@tinas-mbp.home>
2017-04-25 22:01:41 +0100

    first commit

--- swappingScreens/MusicListVC.swift
+++ swappingScreens/MusicListVC.swift
@@ -13,23 +13,29 @@ class MusicListVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        view.backgroundColor = UIColor.yellow
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func backBtnPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func load3rdScreenPressed(_ sender: Any) {
+        let songTitle = "Love Me Now"
+        performSegue(withIdentifier: "PlaySongVC", sender: songTitle)
     }
-    */
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        if let destinationView = segue.destination as? PlaySongVC {
+            
+            if let song = sender as? String {
+                destinationView.selectedSong = song
+                
+            }
+    }
+
+    }
 
 }
--- swappingScreens/PlaySongVC.swift
+++ swappingScreens/PlaySongVC.swift
@@ -9,11 +9,26 @@
 import UIKit
 
 class PlaySongVC: UIViewController {
+    
+    
+    @IBOutlet weak var songTitleLbl: UILabel!
+    
+    private var _selectedSong:String!
+    
+    var selectedSong: String {
+        get {
+            return _selectedSong
+        } set {
+            _selectedSong = newValue
+        
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        songTitleLbl.text = _selectedSong!
 
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
